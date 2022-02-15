eac609d4fc4bac0de73c4e7526afc1e05ffa4cae
Conor Joyce <conor.joyce27@mail.dcu.ie>
2022-02-15 15:55:38 +0000

    updater: don't remove each image, only recreate updated ones

--- updater.sh
+++ updater.sh
@@ -3,10 +3,10 @@
 for dir in ./*; do 
     if [[ -d "$dir" && ${dir} != *"disabled"* ]]; then
         cd "$dir"
-        docker-compose down
-        docker-compose rm -f
         docker-compose pull
         docker-compose up -d
         cd ..
     fi
 done
+
+docker image prune -f
