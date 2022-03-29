f0b533b50a2882e89a87b4eee5a4ba7178405bca
Conor Joyce <conor.joyce27@mail.dcu.ie>
2022-03-29 17:01:58 +0100

    updater.sh: better logging for cronjob

--- updater.sh
+++ updater.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+now=`date +"%Y-%m-%d %T"`
+printf "%s\n---\n" "$now"
 for dir in ./*; do 
     if [[ -d "$dir" && ${dir} != *"disabled"* ]]; then
         cd "$dir"
-        docker-compose pull
-        docker-compose up -d
+        /usr/local/bin/docker-compose pull -q
+        /usr/local/bin/docker-compose up -d
         cd ..
     fi
 done
-
 docker image prune -f
+echo
